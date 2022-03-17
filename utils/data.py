9399febc5a85b201a56bc7727fed813351821844
kuro_ye <erhanaket77@gmail.com>
2022-03-17 22:47:42 +0600

    add week attr

--- utils/data.py
+++ utils/data.py
@@ -14,11 +14,13 @@ def get_time():
     return time_list
 
 class Schedule:
-    def __init__(self, _start_time, _end_time):
+    def __init__(self, _start_time, _end_time, _day_of_the_week):
         self.start_time = _start_time
         self.end_time = _end_time
+        self.day_of_the_week = _day_of_the_week
 
     def get_schedule(self):
         dict = {'start_time': self.start_time,
-                'end_time': self.end_time}
+                'end_time': self.end_time,
+                'day_week': self.day_of_the_week}
         return dict
