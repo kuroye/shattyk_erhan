5ecca9b6d4c669e18f1d7f47c771cb7fe01e1f56
kuro_ye <erhanaket77@gmail.com>
2022-03-18 18:15:02 +0600

    Schedule model class

--- baobao/models.py
+++ baobao/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Schedule(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day_of_the_week = models.CharField(max_length=20)
+
