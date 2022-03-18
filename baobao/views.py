f9e52517517988004de6228d3e584b42baef9045
kuro_ye <erhanaket77@gmail.com>
2022-03-18 19:04:02 +0600

    create Schedule obj

--- baobao/views.py
+++ baobao/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from utils import data
+from .models import Schedule
+
 
 # Create your views here.
 
 def index(request):
-
     if request.method == 'GET':
-
         time_list = data.get_time()
 
-        schedule = data.Schedule('9:00', '10:00', 'Wednesday')
-        schedule_dict = schedule.get_schedule()
+        schedules = Schedule.objects.all()
+        print(schedules)
+
 
         return render(request, 'index.html', context={'time_list': time_list,
-                                                      'schedule': schedule_dict})
+                                                      'schedules': schedules})
