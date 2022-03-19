f0e80f92f25c672937c78a9439b308058dbc9e86
kuro_ye <erhanaket77@gmail.com>
2022-03-19 22:20:04 +0600

    get schedule obj

--- baobao/views.py
+++ baobao/views.py
@@ -9,6 +9,7 @@ def index(request):
     if request.method == 'GET':
         time_list = data.get_time()
 
+        # Schedule.objects.create(start_time='8:00', end_time='10:00', day_of_the_week='Friday')
         schedules = Schedule.objects.all()
         print(schedules)
 
