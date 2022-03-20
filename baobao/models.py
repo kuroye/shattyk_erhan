61418ebf382336dd4c299a99c4f47b7fe09b96a8
kuro_ye <erhanaket77@gmail.com>
2022-03-20 18:07:28 +0600

    change week attr to int

--- baobao/models.py
+++ baobao/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Schedule(models.Model):
     start_time = models.CharField(max_length=20)
     end_time = models.CharField(max_length=20)
-    day_of_the_week = models.CharField(max_length=20)
+    day_of_the_week = models.IntegerField(max_length=20, null=False)
 
